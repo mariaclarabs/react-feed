763e0ee2dea639c55ec19b810a61454f6cafb01a
mariaclarabs <clarabsouza@hotmail.com>
2022-08-26 16:31:04 -0300

    feat: complete sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,11 +10,15 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/mariaclarabs.png" />
         <strong>Maria Souza</strong>
         <span>Software Developer</span>
       </div>
       <footer>
-        <a href='#'>Editar seu perfil</a>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
