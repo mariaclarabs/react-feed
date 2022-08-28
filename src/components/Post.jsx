c7253b97e659ecaf1d36aecc10a18a38cf0b4e79
mariaclarabs <clarabsouza@hotmail.com>
2022-08-28 19:36:19 -0300

    feat: add comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,6 +13,7 @@ export function Post() {
         </div>
         <time title="27 de agosto às 12:00" dateTime="2022-08-27 12:00">Publicado há 1h</time>
       </header>
+
       <div className={styles.content}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta ante libero, eu maximus libero rhoncus at. Pellentesque sollicitudin neque eget orci consequat, quis mollis metus mattis. Fusce commodo consequat odio rhoncus egestas. Pellentesque molestie aliquet dignissim. Maecenas id varius risus, at vehicula nisl. Cras accumsan consectetur dolor eu tincidunt. Curabitur bibendum ex nec sem placerat, ut ultrices dui finibus. Mauris luctus pharetra malesuada. Nullam nec vehicula tortor, eu pulvinar magna. Suspendisse venenatis eros a mattis semper.
@@ -21,6 +22,14 @@ export function Post() {
           <a href="">Link</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário"/>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
