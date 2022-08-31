e3fc2d235cc79c5ed4ace4c3c2e1e04710c5a1ed
mariaclarabs <clarabsouza@hotmail.com>
2022-08-31 15:58:29 -0300

    feat: add comment form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,7 +21,13 @@ export function Post({ author, content, publishedAt }) {
   });
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
+
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório');
   }
 
   function handleCreateComment() {
@@ -37,6 +43,8 @@ export function Post({ author, content, publishedAt }) {
     });
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -70,9 +78,16 @@ export function Post({ author, content, publishedAt }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button 
+            type="submit"
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
